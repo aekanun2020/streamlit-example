7a027ed2ceefdb8e8490ae67c3af4c8d96cdd4d4
www.aekanun.com <68451359+aekanun2020@users.noreply.github.com>
2023-11-21 10:14:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,34 @@
+import streamlit as st
 import urllib.request
 import json
-import os
 import ssl
+import os
 
 def allowSelfSignedHttps(allowed):
     # bypass the server certificate verification on client side
     if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
         ssl._create_default_https_context = ssl._create_unverified_context
 
-allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
+def call_api(data):
+    allowSelfSignedHttps(True)
+    body = str.encode(json.dumps(data))
+    url = 'https://aekanun-deploy-21dec2023.eastus.inference.ml.azure.com/score'
+    api_key = '0QuDIDpd0MmgJuPSbsyAQz2vV4RwvivB'
+    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key), 'azureml-model-deployment': 'aekanun-deploy-21dec2023-1' }
+    req = urllib.request.Request(url, body, headers)
 
-# Request data goes here
-# The example below assumes JSON formatting which may be updated
-# depending on the format your endpoint expects.
-# More information can be found here:
-# https://docs.microsoft.com/azure/machine-learning/how-to-deploy-advanced-entry-script
-data = {}
+    try:
+        response = urllib.request.urlopen(req)
+        result = response.read()
+        return result
+    except urllib.error.HTTPError as error:
+        return "Error: " + str(error)
 
-body = str.encode(json.dumps(data))
+# Streamlit UI
+st.title('API Call Interface')
 
-url = 'https://aekanun-deploy-21dec2023.eastus.inference.ml.azure.com/score'
-# Replace this with the primary/secondary key or AMLToken for the endpoint
-api_key = '0QuDIDpd0MmgJuPSbsyAQz2vV4RwvivB'
-if not api_key:
-    raise Exception("A key should be provided to invoke the endpoint")
-
-# The azureml-model-deployment header will force the request to go to a specific deployment.
-# Remove this header to have the request observe the endpoint traffic rules
-headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key), 'azureml-model-deployment': 'aekanun-deploy-21dec2023-1' }
-
-req = urllib.request.Request(url, body, headers)
-
-try:
-    response = urllib.request.urlopen(req)
-
-    result = response.read()
-    print(result)
-except urllib.error.HTTPError as error:
-    print("The request failed with status code: " + str(error.code))
-
-    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
-    print(error.info())
-    print(error.read().decode("utf8", 'ignore'))
+data_input = st.text_area("Enter your data here")
+if st.button('Call API'):
+    result = call_api(json.loads(data_input))
+    st.text("API Response:")
+    st.write(result)
