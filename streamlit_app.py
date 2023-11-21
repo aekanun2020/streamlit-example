235f5de8bbee9dab3f4722a6877e5e8cbde47ce1
www.aekanun.com <68451359+aekanun2020@users.noreply.github.com>
2023-11-21 12:25:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,21 +5,25 @@ import ssl
 import os
 
 def allowSelfSignedHttps(allowed):
-    # bypass the server certificate verification on client side
+    # Bypass the server certificate verification on client side
     if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
         ssl._create_default_https_context = ssl._create_unverified_context
 
-def call_api(data):
+def call_api(text):
     allowSelfSignedHttps(True)
+
+    # สร้างข้อมูลที่จะส่งไปยัง API
+    data = {"question": text}
     body = str.encode(json.dumps(data))
+
     url = 'https://aekanun-deploy-21dec2023.eastus.inference.ml.azure.com/score'
     api_key = '0QuDIDpd0MmgJuPSbsyAQz2vV4RwvivB'
-    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+    headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + api_key}
     req = urllib.request.Request(url, body, headers)
 
     try:
         response = urllib.request.urlopen(req)
-        # ใช้ .decode('utf-8') เพื่อแปลงจาก bytes เป็น string
+        # แปลงผลลัพธ์จาก bytes เป็น string
         result = response.read().decode('utf-8')
         return result
     except urllib.error.HTTPError as error:
@@ -28,22 +32,15 @@ def call_api(data):
 # Streamlit UI
 st.title('สวัสดีครับ ผมครูเอ้เอง ครับ มาในเวอร์ชั่นของ AI')
 
-data_input = st.text_area("ท่านอยากรู้อะไรในเรื่องที่ครูได้บรรยายไปบ้างครับ")
+# รับข้อมูลเป็นข้อความธรรมดา
+text_input = st.text_area("ท่านอยากรู้อะไรในเรื่องที่ครูได้บรรยายไปบ้างครับ")
 if st.button('สอบถามครู'):
     try:
-        json_data = json.loads(data_input)
-        result = call_api(json_data)
+        # ส่งข้อความไปยัง API
+        result = call_api(text_input)
 
-        # แปลงจาก JSON string เป็น Python object
-        result_obj = json.loads(result)
-
-        # ใช้ Expander สำหรับแสดงผลข้อมูล
+        # แสดงผลลัพธ์ในรูปแบบ JSON
         with st.expander("API Response:"):
-            st.json(result_obj)  # ใช้ st.json สำหรับแสดงผล JSON ที่จัดรูปแบบแล้ว
-    except json.JSONDecodeError:
-        st.error("Invalid JSON input. Please enter valid JSON data.")
-
-
-
-
-
+            st.json(json.loads(result))  # หรือ st.write ถ้าไม่ต้องการใช้ JSON
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
